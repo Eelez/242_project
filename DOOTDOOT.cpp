66528997667039bbb42ec61cd927e5bc2ba56019
Samuel <hansa103@student.otago.ac.nz>
2020-09-04 17:00:11 +1200

    testing

---
+++ DOOTDOOT.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Samuel on 4/09/2020.
+//
+
+#include "DOOTDOOT.h"
---
+++ DOOTDOOT.h
@@ -0,0 +1,14 @@
+//
+// Created by Samuel on 4/09/2020.
+//
+
+#ifndef INC_242_PROJECT_DOOTDOOT_H
+#define INC_242_PROJECT_DOOTDOOT_H
+
+
+class DOOTDOOT {
+
+};
+
+
+#endif //INC_242_PROJECT_DOOTDOOT_H
