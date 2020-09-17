15d97360c190e1654097477a70d59350f0ec7ea9
Eelez <70737051+Eelez@users.noreply.github.com>
2020-09-17 17:45:23 +1200

    Update tree.h

--- tree.h
+++ tree.h
@@ -5,4 +5,13 @@
 #ifndef INC_242_PROJECT_BST_H
 #define INC_242_PROJECT_BST_H
 
+typedef struct treenode *tree;
+
+extern void tree_free(tree t);
+extern void tree_inorder(tree t, void f(char *str));
+extern tree tree_insert(tree t, char *str);
+extern tree tree_new();
+extern void tree_preorder(tree t, void f(char *str));
+extern int tree_search(tree t, char *str);
+
 #endif //INC_242_PROJECT_BST_H
